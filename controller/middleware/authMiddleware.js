4eb4c645b537e97d23c29b9dad63f4662aeec83d
ndubuisiifeanyi828 <ndubuisiifeanyi828@gmail.com>
2025-12-03 12:42:43 -0800

    update

---
+++ controller/middleware/authMiddleware.js
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken';
+import cohortfour from "../../model/usermodel.js";
+
+export const protect = async (req, res, next) => {
+    let token;
+
+    if (
+        req.headers.authorization &&
+        req.headers.authorization.startsWith('Bearer ')
+    ){
+        try {
+            token = req.headers.authorization.split("")[1];
+            const decoded = jwt.verify(token, process.env.
+                SECRET_KEY);
+            req.user = await cohortfour.findById(decoded.id).select
+            ("-password");
+            next();
+        } catch (error) {
+            return res.status(401).json({message: 'Not authorized, invalid token'});
+        }
+    }
+
+    if (!token) {
+        return res.status(401).json({message: 'Not authorized, no token'});
+    }
+}
--- route/userroute.js
+++ route/userroute.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { createStudents, deleteUser, getAllStudents,getUserByid,loginUser, updateUser } from "../controller/usercontroller.js";
+import { protect } from "../controller/middleware/authmiddleware.js";
 const router = express.Router()
 router.post('/register',createStudents)
-router.get('/',getAllStudents)
+router.get('/',protect,getAllStudents)
 router.get('/:id',  getUserByid)
 router.post('/login', loginUser)
 router.put('/update/:id',updateUser)
